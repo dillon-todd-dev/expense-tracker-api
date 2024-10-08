96bae552b9480d83c00b1cd929e5807445b62fc7
dillon-todd-dev <dillontodd.dev@gmail.com>
2024-10-08 13:09:39 -0500

    setup prisma and postgresql

--- Dockerfile
+++ Dockerfile
@@ -2,14 +2,20 @@ FROM node:22.7.0-slim
 
 WORKDIR /app
 
+COPY prisma .
+
 COPY package*.json .
+
+RUN apt-get update -y && apt-get install -y openssl
 
 RUN npm install
 
 COPY . .
 
+RUN npx prisma generate
+
 RUN npm run build
 
 EXPOSE 3000
 
-CMD [ "npm", "run", "start:prod" ]
+CMD [ "npm", "run", "start:migrate:prod" ]
---
+++ prisma/migrations/20241008180439_init/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "user_id" TEXT NOT NULL,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "dateOfBirth" DATE NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("user_id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
