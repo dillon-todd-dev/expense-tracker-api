214b1aa2f78ff924d90b46999380d94652add079
dillon-todd-dev <dillontodd.dev@gmail.com>
2024-11-06 20:07:11 -0600

    add crud endpoints for users

--- src/services/usersService.js
+++ src/services/usersService.js
@@ -16,8 +16,22 @@ const createUser = async (data) => {
     return user;
 }
 
+const updateUser = async (id, data) => {
+    const userId = utils.convertToObjectId(id);
+    const query = { _id: userId };
+    return await app.db.collection('users').replaceOne(query, data);
+}
+
+const deleteUser = async (id) => {
+    const userId = utils.convertToObjectId(id);
+    const query = { _id: userId };
+    return await app.db.collection('users').deleteOne(query);
+}
+
 module.exports = {
     getUsers,
     getUserById,
-    createUser
+    createUser,
+    updateUser,
+    deleteUser
 };
